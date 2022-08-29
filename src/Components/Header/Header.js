8aea31c14ce66663566e37aabd33a90a25ca2f09
Lucasbelich <lucasbelich1@gmail.com>
2022-08-29 14:31:56 -0300

    fix

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Lucas Belich</h1>
-        <h5 className="text-light">Frontend Developer</h5>
+        <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocials />
 
